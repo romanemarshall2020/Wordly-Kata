8634b62df7e75fe2b4b6b9ce003dab3b011f54e5
Marshall <romane.marshall@accenture.com>
2023-06-08 12:05:21 -0400

    working on making text appear in box when submit button is pressed

--- js/index.js
+++ js/index.js
@@ -1,61 +1,99 @@
 import Wordle from "./wordle.js";
-let wordle 
+let wordle;
 
-endGameButton.addEventListener("click", function() {
-    alert("game has ended")
-    document.getElementById("gameStarted").style.visibility="hidden"
-    document.getElementById("startGameButton").style.visibility="visible"
-    initialize()
-})
-
-
-startGameButton.addEventListener("click", function() {
-        console.log("game started!")
-        document.getElementById("gameStarted").style.visibility="visible"
-        document.getElementById("startGameButton").style.visibility="hidden"
-        initialize()
+endGameButton.addEventListener("click", function () {
+  alert("game has ended");
+  document.getElementById("gameStarted").style.visibility = "hidden";
+  document.getElementById("startGameButton").style.visibility = "visible";
+  initialize();
 });
 
-guessButton.addEventListener("click", function() {
-        console.log("got the guess")
-        playGame();
+startGameButton.addEventListener("click", function () {
+  console.log("game started!");
+  document.getElementById("gameStarted").style.visibility = "visible";
+  document.getElementById("startGameButton").style.visibility = "hidden";
+  initialize();
 });
 
+guessButton.addEventListener("click", function () {
+  console.log("got the guess");
+  playGame();
+});
 
 function initialize() {
-    wordle = new Wordle
-    console.log("Welcome to Wordle! In this game you will have 6 tries to guess the word")
-        // console.log(wordle.randomlyChosenWord)
-        wordle.turnToUnderscore(wordle.randomlyChosenWord)
+  wordle = new Wordle();
+  console.log(
+    "Welcome to Wordle! In this game you will have 6 tries to guess the word"
+  );
+  wordle.turnToUnderscore(wordle.randomlyChosenWord);
+  
 }
-
 
 function playGame() {
-    console.log("inside playe game ")
-    let isGameOver = false;
-        let guess = document.getElementById("myInput").value
-        document.getElementById("myInput").value = null
-         guess = wordle.checkInput(guess)
-        if(guess !== 0){
-            isGameOver = wordle.isGameWon(guess)
-        
-            if(!isGameOver){
-            if(wordle.guessesRemaining() === 0){
-                alert("You have no more guesses. You lose!")
-                isGameOver = true
-                initialize()
-            } else {   
-            wordle.guessesRemaining()
-            wordle.revealLetter(guess)
+  console.log("inside playe game ");
+  let isGameOver = false;
+  let guess = document.getElementById("myInput").value;
+  
+  for(let i = 0; i < guess.length; i++) {
+    for(let j = 0; j < 5; j++){
+    document.getElementById("box1").append(guess[i])
+    }
+  }
+  document.getElementById("myInput").value = null;
+  guess = wordle.checkInput(guess);
+  if (guess !== 0) {
+    isGameOver = wordle.isGameWon(guess);
 
-            }
-        } else {
-            alert("Congratulations you WON!!") 
-            initialize()}
-        }
-        
+    if (!isGameOver) {
+      if (wordle.guessesRemaining() === 0) {
+        alert("You have no more guesses. You lose!");
+        isGameOver = true;
+        initialize();
+      } else {
+        wordle.guessesRemaining();
+        wordle.revealLetter(guess);
+      }
+    } else {
+      alert("Congratulations you WON!!");
+      initialize();
+    }
+  }
 }
-export {playGame}
+
+
+// document.getElementById("box1")
 
 
 
+
+// function drawBox(container, row, col, letter = "") {
+//     console.log("inside of drawbox")
+//     const box = document.createElement("div");
+//     box.className = "boxes";
+//     box.id = `boxes${row}${col}`;
+//     box.textContent = letter;
+//     container.appendChild(box);
+    
+//     return box
+// }
+
+// function drawGrid(container) {
+//     console.log("inside of drawgrid")
+//     const grid = document.createElement("div");
+//     grid.className = "rows";
+//     for(let i = 0; i < 6; i++) {
+//         for(let j = 0; j < 5; j++) {
+//             drawBox(grid, i, j)
+//         }
+//     }
+//     container.appendChild(grid)
+// }
+//     function startup() {
+//         const wordContainer = document.getElementById("wordContainer")
+//         drawGrid(wordContainer)
+    
+//     }
+    
+
+
+export { playGame };
--- js/wordle.js
+++ js/wordle.js
@@ -14,119 +14,99 @@ class Wordle {
       "giant",
       "spare",
       "style",
-      "vital"
+      "vital",
     ];
     this.maxGuess = 6;
-    this.guess = ""
+    this.guess = "";
     this.incorrectGuesses = 0;
     this.randomlyChosenWord = this.displayRandomWord();
-    this.hiddenWord = [ '_', '_', '_', '_', '_' ];
-
-    
+    this.hiddenWord = ["_", "_", "_", "_", "_"];
   }
-  revealLetter( guess ){
+  revealLetter(guess) {
     let result = "";
-    // let guessArray = guess.split("");
-   
- 
-  
-      for(let i = 0; i < guess.length; i++){
-        if(guess.charAt(i) === this.randomlyChosenWord.charAt(i)){
-          result += guess.charAt(i).toUpperCase()
-        }
-        else if (this.randomlyChosenWord.includes(guess.charAt(i))){
-          result += guess.charAt(i).toLowerCase()
-        } else {
-          result += "_"
-        }
-        
-      }
-      console.log(result)
-      return result
 
-   
+    for (let i = 0; i < guess.length; i++) {
+      if (guess.charAt(i) === this.randomlyChosenWord.charAt(i)) {
+        result += guess.charAt(i).toUpperCase();
+      } else if (this.randomlyChosenWord.includes(guess.charAt(i))) {
+        result += guess.charAt(i).toLowerCase();
+      } else {
+        result += "_";
+      }
+    }
+    console.log(result);
+    return result;
   }
   turnToUnderscore(randomlyChosenWord) {
-    if(typeof randomlyChosenWord === "string") {
+    if (typeof randomlyChosenWord === "string") {
       let wordArray = randomlyChosenWord.split("");
       for (let i = 0; i < wordArray.length; i++) {
         wordArray[i] = "_";
-       
       }
-      console.log(wordArray)
-    return wordArray;
+      console.log(wordArray);
+      return wordArray;
     }
-    
   }
 
   displayRandomWord() {
-    let randomlyChosenWord = this.randomWords[Math.floor(Math.random() * this.randomWords.length)];
-    let max = this.maxGuess
-    let incorrect = this.incorrectGuesses
-      console.log(`You have ${max - incorrect} attempts left.`);
+    let randomlyChosenWord = this.randomWords[
+      Math.floor(Math.random() * this.randomWords.length)
+    ];
+    let max = this.maxGuess;
+    let incorrect = this.incorrectGuesses;
+    console.log(`You have ${max - incorrect} attempts left.`);
     console.log(randomlyChosenWord);
-    this.turnToUnderscore(randomlyChosenWord)
+    this.turnToUnderscore(randomlyChosenWord);
     return randomlyChosenWord;
   }
 
-  
   guessesRemaining() {
-    console.log(`You have ${this.maxGuess - this.incorrectGuesses} attempts left.`);
-    return this.maxGuess - this.incorrectGuesses
+    console.log(
+      `You have ${this.maxGuess - this.incorrectGuesses} attempts left.`
+    );
+    return this.maxGuess - this.incorrectGuesses;
   }
-
 
   playerGuess() {
     let guess = document.getElementById("myInput").value;
-    guess = this.checkInput(guess)
-    
-
-
-    console.log(this.incorrectGuesses)
+    guess = this.checkInput(guess);
+    console.log(this.incorrectGuesses);
     return guess;
-   
-  }
- 
-  isGameWon(guess){
- 
-        if(guess === this.randomlyChosenWord){
-          console.log("game over you won")
-          return true
-        } else {
-          return false
-        }
   }
 
-  
+  isGameWon(guess) {
+    if (guess === this.randomlyChosenWord) {
+      console.log("game over you won");
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   checkInput(guess) {
     let isValidWord = false;
     while (!isValidWord) {
-      
-      if(typeof guess === "string"){
-      if (guess.length === 5 && guess.match(/[a-z]/i)) {
-          this.incorrectGuesses++
-        isValidWord = true;
-      } else if (guess === "") {
-        console.log("Nothing was entered");
-        alert( "Nothing was entered please");
-        return 0
-        
-      } else {
-        guess = question("Error: Please guess a valid letter.");
+      if (typeof guess === "string") {
+        if (guess.length === 5 && guess.match(/[a-z]/i)) {
+          this.incorrectGuesses++;
+          isValidWord = true;
+        } else if (guess === "") {
+          console.log("Nothing was entered");
+          alert("Nothing was entered please");
+          return 0;
+        } else {
+          guess = question("Error: Please guess a valid letter.");
+        }
       }
-    
     }
-    } 
-    console.log("assigning guess")
+    console.log("assigning guess");
     console.log("This is guess111: " + guess);
     console.log("This is random word: " + this.randomlyChosenWord);
 
-    console.log("assigned guess")
-    console.log(`guess: ${guess}`)
-    return guess
+    console.log("assigned guess");
+    console.log(`guess: ${guess}`);
+    return guess;
   }
-
-
 }
 
 export default Wordle;
